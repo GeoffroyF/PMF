cbcd95e754080917c605e4a6fc6100ed5cf6aa86
ludwhe <heililouis@gmail.com>
2017-11-17 13:45:37 +0100

    Changes to be committed: 	deleted:    Arduino/main.ino 	new file:   Arduino/main/main.ino

--- Arduino/main.ino
+++
@@ -1,9 +0,0 @@
-void setup ()
-{
-    //  SETUP
-}
-
-void loop ()
-{
-    //  LOOP
-}
---
+++ Arduino/main/main.ino
@@ -0,0 +1,13 @@
+#define TH_INPUT_PIN  A0
+
+void setup ()
+{
+    //  SETUP
+   pinMode(TH_INPUT_PIN, INPUT);
+}
+
+void loop ()
+{
+    //  LOOP
+    
+}
